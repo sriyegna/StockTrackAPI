5479096c81e75147652a2e05cc1b3a8be38092be
sriyegna <sriyegna@users.noreply.github.com>
2020-02-12 15:06:22 -0500

    removed notes/useless comments

--- SECDataExtraction.py
+++ SECDataExtraction.py
@@ -8,36 +8,10 @@ import time
 import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 import pandas as pd
-
-#Notes
-#rowArr
-#0 cashcashequivalentsandshortterminvestments
-#1 commonstock
-#2 ebit
-#3 grossprofit
-#4 incomebeforetaxes
-#5 incometaxes
-#6 netincome
-#7 operatingprofit
-#8 totallongtermdebt
-#9 totalrevenue
-#10 totalshorttermdebt
-#11 capitalexpenditures
-#12 cfdepreciationamortization
-#13 changeincurrentassets
-#14 changeincurrentliabilities
-#15 workingCapital
-#16 eBITDA
-#17 taxRate
-#18 interestExpense
-
-
 #Globals
 EdgarKey = 'a4585b035f8bb44392e348073ec85ca2'
 fieldsToGather = ["cashcashequivalentsandshortterminvestments", "commonstock", "ebit", "grossprofit", "incomebeforetaxes", "incometaxes", "netincome", "operatingprofit", "totallongtermdebt",
                   "totalrevenue", "totalshorttermdebt", "capitalexpenditures", "cfdepreciationamortization", "changeincurrentassets", "changeincurrentliabilities"]
-
-
 
 def connectToDb():
     db = mysql.connector.connect(
@@ -48,16 +22,12 @@ def connectToDb():
     )
     return db
 
-
-
 url = 'https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download'
 exchanges = ['NASDAQ', 'AMEX', 'NYSE']
 df = pd.concat([pd.read_csv(url.format(ex)) for ex in exchanges]).dropna(how='all', axis=1)
 df = df.rename(columns=str.lower).set_index('symbol').drop('summary quote', axis=1).dropna(subset=['marketcap'], axis='rows')
 df = df[~df.index.duplicated()]
 tickers = df.index
-#print(tickers)
-#tickers = ["AAPL"]
 
 for ticker in tickers:
     try:
@@ -65,8 +35,6 @@ for ticker in tickers:
         data_BSC = get(url).json()
         url = ' https://datafied.api.edgar-online.com/v2/corefinancials/ann?Appkey=2d2d5cb64a16d58eb8d7eac5a2100090&fields=CashFlowStatementConsolidated&primarysymbols=' + ticker + '&numperiods=12&activecompanies=false&deleted=false&sortby=primarysymbol%20asc&debug=false'
         data_CFSC = get(url).json()
-        #print(data_BSC['result']['totalrows'])
-        #print(data_CFSC['result']['totalrows'])
         if (data_BSC['result']['totalrows'] == data_CFSC['result']['totalrows']):
             #Same # of rows of data from both calls
             arrayLen = data_CFSC['result']['totalrows']
@@ -110,7 +78,6 @@ for ticker in tickers:
             try:
                 db = connectToDb()
                 mycursor = db.cursor(buffered=True)
-
                 Temp_Year = int(datetime.now().year) - 1 - i
                 id = ticker + str(Temp_Year)
                 print(id)
